738778490899eb781ef1acd1a7502e40c2b56467
Tyler Long <tyler4long@gmail.com>
2017-09-04 08:46:06 +0800

    Remove babel-plugin-transform-remove-strict-mode

--- webpack.config.js
+++ webpack.config.js
@@ -22,12 +22,7 @@ const config = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: [
-              'transform-remove-strict-mode' // in order to make mermaid work
-            ]
-          }
+          loader: 'babel-loader'
         }
       },
       {
