2f28257ab9e058314a7f9736cf966f85d64fc7ab
Tyler Long <tyler4long@gmail.com>
2017-12-25 14:50:22 +0800

    Customize gantt diagram axisFormat

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import mermaid from 'mermaid'
 
-mermaid.initialize({ theme: 'forest' })
-// mermaid.initialize({ theme: 'default' })
+mermaid.initialize({
+  theme: 'forest',
+  gantt: { axisFormatter: [
+    ['%Y-%m-%d', (d) => {
+      return d.getDay() === 1
+    }]
+  ] }
+})
